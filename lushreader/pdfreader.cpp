e61bcaa77b77d18059a4fcc7b63041e60b78f532
Sean Ford <seanford@protonmail.com>
2024-08-06 13:50:09 -0700

    Add file stream opening function

--- lushreader/pdfreader.cpp
+++ lushreader/pdfreader.cpp
@@ -1,5 +1,14 @@
 #include "pdfreader.h"
 
+void PDFReader::loadPDFFIle(const std::string& filePath) {
+    m_fileStream.open(filePath);
+
+    if (!m_fileStream) {
+        std::println("ERROR: File could not be opened");
+    }
+}
+
 bool PDFReader::isPDFFilePath(const std::string_view filePath) {
     return filePath.size() > 4 && filePath.ends_with(".pdf");
 }
+
--- lushreader/pdfreader.h
+++ lushreader/pdfreader.h
@@ -1,6 +1,12 @@
+#include <fstream>
+#include <print>
+#include <string>
 #include <string_view>
 
 class PDFReader {
     public:
+        void loadPDFFIle(const std::string& filePath);
         static bool isPDFFilePath(std::string_view possibleFilePath); 
+    private:
+        std::ifstream m_fileStream{};
 };
