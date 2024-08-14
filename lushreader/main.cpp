81825393ce7a16d90882c04c095f1fa8f8785c7d
Sean Ford <seanford@protonmail.com>
2024-08-14 13:58:49 -0700

    Open pdf from CLI

--- lushreader/main.cpp
+++ lushreader/main.cpp
@@ -1,4 +1,15 @@
-int main() {
+#include <print>
+#include "pdfreader.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::println("Proper usage: {} $filePath", argv[0]);
+    }
+
+    PDFReader reader{};
+    reader.loadPDFFile(argv[1]);
+    reader.readHeader();
+
     return 0;
 }
 
