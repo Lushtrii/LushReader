0e2086650e7b9253ed1bd01598eba9c0eee43370
Sean Ford <seanford@protonmail.com>
2024-08-19 11:59:33 -0700

    Add getters to PDFReader class

--- lushreader/pdfreader.h
+++ lushreader/pdfreader.h
@@ -11,6 +11,8 @@ class PDFReader {
         explicit PDFReader(const std::string& filePath);
         void loadPDFFile(const std::string& filePath);
         void readHeader();
+        const std::string& getHeader() const {return m_headerVersion;};
+        const std::ifstream& getFileStream() const {return m_fileStream;};
         static bool isPDFFilePath(std::string_view possibleFilePath); 
     private:
         std::ifstream m_fileStream{};
